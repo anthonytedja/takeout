1ceb8f58dab97cddb4b2fe179b2cd74bb2ee20e7
Anthony Tedja <anthonytedja27@gmail.com>
2021-10-16 15:32:02 -0400

    Add Restaurants

--- scripts.js
+++ scripts.js
@@ -6,11 +6,35 @@ var snack = document.getElementById("snack");
 var fastfood = document.getElementById("fastfood");
 var restaurant = document.getElementById("restaurant");
 
-// Random Generate The Number From 0 - 256
-function genRandom() {
-    var randomNum = Math.floor(Math.random() * 256).toString(16).padStart(2, 0);
+const snacklist = ["ChungChun Corn Dogs", "Smokes Poutinerie", "Murphys Ice Cream",
+    "I <3 Boba", "Real Fruit", "Gong Cha", "Starbucks", "Tims", "McDonalds",
+    "Sugar Marmalade", "Supermoon", "Cocos", "Baskin Robbins", "Chatime",
+    "Hazukido", "Juicy Dumplings"];
 
-    return randomNum;
+const fastfoodlist = ["McDonalds", "Philthy Phillys", "Manousha", "H Mart",
+    "Nations Kitchen", "Burger Factory", "Alf Hana", "Ricks Good Eats",
+    "Ice Q", "Little Caesars", "Chipotle", "Wendys", "Five Guys", "KFC",
+    "Taco Bell", "Costco", "IKEA", "Pizza Nova", "Popeyes", "Loblaws",
+    "Fortune Dragon", "Pizza Pizza", "Panagio Breakfast", "Dominoes",
+    "Bourbon St Grill", "Firehouse Subs", "Dixie Food Court", "Cora",
+    "Jimmy The Greek", "Harveys", "Subway", "Mary Browns", "Sushi Park",
+    "Mississauga Marketplace F&C", "Davs HotSpot", "La Carnita",
+    "Birria Catrina"];
+
+const restaurantlist = ["Mihito Poke Bowls", "Poke Guys", "Jinzakaya", "Pho",
+    "Younge St Warehouse", "Kelseys", "ManDooHyang", "K Pocha", "Sikgaek",
+    "Bubble Republic", "Katsuya", "Nandos", "Song Cook", "Kamen Ramen",
+    "Kinton Ramen", "Social Eatery", "Owl of Minerva", "Chucks Roadhouse",
+    "168 Sushi", "Goodfellas", "El Mariachi", "Swiss Chalet", "Iron Chef",
+    "Turtle Jacks", "Wind Sushi", "Heart Sushi", "Buk Chang Dong",
+    "Chicken in the Kitchen"];
+
+function generateOrder() {
+    snack.textContent = "SNACK : " + snacklist[Math.floor(Math.random() * snacklist.length)].toUpperCase();
+    fastfood.textContent = "FAST FOOD : " + fastfoodlist[Math.floor(Math.random() * fastfoodlist.length)].toUpperCase();
+    restaurant.textContent = "RESTAURANT : " + restaurantlist[Math.floor(Math.random() * restaurantlist.length)].toUpperCase();
+
+    changeBgColor();
 }
 
 // Change The Background Color Base On The Random Color Code
@@ -28,95 +52,28 @@ function changeBgColor() {
     // Result Displaying
     main[0].style.backgroundColor = "#" + hexValue;
 
-    exchangeRgb(hexX, hexY, hexZ);
+    var rgbX = parseInt(hexX, 16);
+    var rgbY = parseInt(hexX, 16);
+    var rgbZ = parseInt(hexX, 16);
 
-}
-
-// Exchange The Hex Value To RGB Value
-function exchangeRgb(x, y, z){
-
-    var rgbX = parseInt(x, 16);
-    var rgbY = parseInt(y, 16);
-    var rgbZ = parseInt(z, 16);
-
-    // Result Displaying
-    snack.innerHTML = "SNACK : (" + rgbX + ", " + rgbY + ", " + rgbZ + ")";
-
-    exchangeHsl(rgbX, rgbY, rgbZ);
-    exchangeCmyk(rgbX, rgbY, rgbZ);
-
-}
-
-// Exchange The RGB Value To HSL Value
-function exchangeHsl(r, g, b){
-
-    var rgbR = r /= 255;
-    var rgbG = g /= 255;
-    var rgbB = b /= 255;
-
-    var hue;
-    var saturation;
-    var lightness;
-
-    var rgbArray = [rgbR, rgbG, rgbB];
+    var rgbR = rgbX /= 255;
+    var rgbG = rgbY /= 255;
+    var rgbB = rgbZ /= 255;
 
     var rgbMax = Math.max(rgbR, rgbG, rgbB); // Maximum Value Of RGB
     var rgbMin = Math.min(rgbR, rgbG, rgbB); // Minimum Value Of RGB
 
-    // Calculate The Lightness Value
     var lightness = Math.round(((rgbMin + rgbMax) / 2) * 100);
-
-    // Calculate The Saturation Value
-    if (rgbMax == rgbMin){
-        saturation = 0;
-    } else if (lightness < 50){
-        saturation = Math.round((rgbMax - rgbMin) / (rgbMax + rgbMin) * 100);
-    } else if (lightness > 50){
-        saturation = Math.round((rgbMax - rgbMin) / (2.0 - rgbMax - rgbMin) * 100);
-    } else if (lightness == 50){
-        saturation = Math.round((rgbMax - rgbMin) / (rgbMax + rgbMin) * 100);
-    }
-
-    // Calculate The Hue Value
-    if (rgbMax == rgbR){
-        hue = (rgbG - rgbB) / (rgbMax - rgbMin);
-    } else if (rgbMax == rgbG){
-        hue = 2.0 + (rgbB - rgbR) / (rgbMax - rgbMin);
-    } else if (rgbMax == rgbB){
-        hue = 4.0 + (rgbR - rgbG) / (rgbMax - rgbMin);
-    }
-
-    hue = Math.round(hue * 60);
-
-    if (hue < 0){
-        hue = hue + 360;
-    }
-
-    // Result Displaying
-    fastfood.innerHTML = "FAST FOOD : (" + hue + "°, " + saturation + "%, " + lightness + "%)";
 
     changeFontColor(lightness);
 
 }
 
-// Exchange The RGB Value To HSL Value
-function exchangeCmyk(r, g, b){
+// Random Generate The Number From 0 - 256
+function genRandom() {
+    var randomNum = Math.floor(Math.random() * 256).toString(16).padStart(2, 0);
 
-    var rgbR = r /= 255;
-    var rgbG = g /= 255;
-    var rgbB = b /= 255;
-
-    var K = 1 - Math.max(rgbR, rgbG, rgbB);
-
-    var C = Math.round((1 - rgbR - K) / (1 - K) * 100);
-    var M = Math.round((1 - rgbG - K) / (1 - K) * 100);
-    var Y = Math.round((1 - rgbB - K) / (1 - K) * 100);
-
-    K = Math.round(K * 100);
-
-    // Result Displaying
-    restaurant.innerHTML = "RESTAURANT : (" + C + "%, " + M + "%, " + Y + "%, " + K + "%)";
-
+    return randomNum;
 }
 
 // Change The Font Color Base on The Nightness of The Background
